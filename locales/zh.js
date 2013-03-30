a5dc1d0dda2aa7d61c4f776c1493d84ad7598b8a
David Frank <bitinn@gmail.com>
2013-03-30 16:22:43 +0800

    get some of that bootstrap goodness, steal their template example as well;

--- locales/zh.js
+++ locales/zh.js
@@ -1,4 +1,9 @@
 {
 	"Site Name": "站点名称",
-	"Welcome to ": "欢迎来到"
+	"Welcome to ": "欢迎来到",
+	"Asked": "Asked",
+	"Asked - The finest Q&amp;A service.": "Asked - The finest Q&amp;A service.",
+	"Asked is the Q&amp;A service you really need, I mean, who doesn&rsquo;t have a couple of questions on their mind?": "Asked is the Q&amp;A service you really need, I mean, who doesn&rsquo;t have a couple of questions on their mind?",
+	"Asked - The finest Q&amp;A service": "Asked - The finest Q&amp;A service",
+	"Asked is the Q&amp;A service you really need, I mean, who doesn&rsquo;t have a couple of questions answers on their mind?": "Asked is the Q&amp;A service you really need, I mean, who doesn&rsquo;t have a couple of questions answers on their mind?"
 }
