925d81e82798eec650f7b92a4f92657b32231f9e
David Frank <bitinn@gmail.com>
2013-04-01 15:50:45 +0800

    basic oauth testing

--- locales/en.js
+++ locales/en.js
@@ -1,4 +1,25 @@
 {
 	"Site Name": "Site Name",
-	"Welcome to ": "Welcome to "
+	"Asked - The world&rsquo;s finest Q&amp;A service": "Asked - The world&rsquo;s finest Q&amp;A service",
+	"Asked is the Q&amp;A service you really need, I mean, who doesn&rsquo;t have a couple of questions and answers on their mind?": "Asked is the Q&amp;A service you really need, I mean, who doesn&rsquo;t have a couple of questions and answers on their mind?",
+	"Project Asked": "Project Asked",
+	"Features": "Features",
+	"Great Q&amp;A": "Great Q&amp;A",
+	"Order by": "Order by",
+	"Usefulness": "Usefulness",
+	"Creativity": "Creativity",
+	"Popularity": "Popularity",
+	"Even Better Q&amp;A": "Even Better Q&amp;A",
+	"Irrelevance": "Irrelevance",
+	"Obscurity": "Obscurity",
+	"Spam confidence level": "Spam confidence level",
+	"Q&amp;A sucks.": "Q&amp;A sucks.",
+	"Because most questions are boring, while answers are full of incorrect, irrelevant and out-dated information. We save you from those tasteless Q&amp;As, automagically.": "Because most questions are boring, while answers are full of incorrect, irrelevant and out-dated information. We save you from those tasteless Q&amp;As, automagically.",
+	"Sign me up": "Sign me up",
+	"Q&amp;A what matters.": "Q&amp;A what matters.",
+	"And we mean it, all Q&amp;A lives for a 24-hours period by default: once their lifespan limit are reached, our system evaporates them, no backup or recovery.": "And we mean it, all Q&amp;A lives for a 24-hours period by default: once their lifespan limit are reached, our system evaporates them, no backup or recovery.",
+	"Feeling brave": "Feeling brave",
+	"Q&amp;A forever.": "Q&amp;A forever.",
+	"Here comes the game: every Q&amp;A&rsquo;s lifespan depends on its scores - higher scores means longer lifespan. Your goal is to make your Q&amp;As worthy of survival.": "Here comes the game: every Q&amp;A&rsquo;s lifespan depends on its scores - higher scores means longer lifespan. Your goal is to make your Q&amp;As worthy of survival.",
+	"Challenge accepted": "Challenge accepted"
 }
--- models/user.js
+++ models/user.js
@@ -6,6 +6,7 @@
 module.exports = function(db, oauth, config){
 
   var user = {};
+  var prefix = 'user';
 
   // oauth setup - twitter
   oauth.passport.use(new oauth.twitter({
@@ -13,26 +14,7 @@ module.exports = function(db, oauth, config){
       consumerSecret: config.twitter.consumerSecret,
       callbackURL: config.twitter.callbackURL
     },
-    function(token, tokenSecret, profile, done) {
-
-      //var params = {};
-      //console.log(profile);
-      return done(null, profile);
-
-      /*
-      User.findOne(params, function (err, user) {
-        if (err) { return done(err); }
-        if (!user) {
-          return done(null, false, { message: 'Incorrect username.' });
-        }
-        if (!user.validPassword(password)) {
-          return done(null, false, { message: 'Incorrect password.' });
-        }
-        return done(null, user);
-      });
-      */
-
-    }
+    twitterUser
   ));
 
   // oauth setup - weibo
@@ -41,26 +23,7 @@ module.exports = function(db, oauth, config){
       clientSecret: config.weibo.clientSecret,
       callbackURL: config.weibo.callbackURL
     },
-    function(token, tokenSecret, profile, done) {
-
-      //var params = {};
-      //console.log(profile);
-      return done(null, profile);
-
-      /*
-      User.findOne(params, function (err, user) {
-        if (err) { return done(err); }
-        if (!user) {
-          return done(null, false, { message: 'Incorrect username.' });
-        }
-        if (!user.validPassword(password)) {
-          return done(null, false, { message: 'Incorrect password.' });
-        }
-        return done(null, user);
-      });
-      */
-
-    }
+    weiboUser
   ));
 
   // what to store/retrieve from session
@@ -79,6 +42,100 @@ module.exports = function(db, oauth, config){
   user.twitterCallback = oauth.passport.authenticate('twitter', { successRedirect: '/account', failureRedirect: '/login' });
   user.weiboCallback = oauth.passport.authenticate('weibo', { successRedirect: '/account', failureRedirect: '/login' });
 
+  // wrapper oauth handler
+  function twitterUser(token, tokenSecret, profile, done) {
+
+    if(!profile) {
+      return done(err);
+    }
+
+    var user = {
+      provider: profile.provider,
+      id: profile.id,
+      username: profile.username,
+      nickname: profile.displayName,
+      avatar: profile.photos[0].value
+    }
+
+    oauthUser(token, tokenSecret, user, done);
+
+  }
+
+  function weiboUser(token, tokenSecret, profile, done) {
+
+    if(!profile) {
+      return done(err);
+    }
+
+    var user = {
+      provider: profile.provider,
+      id: profile.id,
+      username: profile.username,
+      nickname: profile.nickname,
+      avatar: profile.avatarUrl
+    }
+
+    oauthUser(token, tokenSecret, user, done);
+
+  }
+
+  // handle oauth result
+  function oauthUser(token, tokenSecret, profile, done) {
+
+    var params = profile;
+
+    // the local profile id of this user
+    params.local = prefix + '.' + params.provider + '.' + params.id;
+
+    matchUser(params, function(err, resp){
+
+      if(resp == 1) {
+
+        loginUser(params, function(err, resp){
+          var user = JSON.parse(resp);
+          return done(null, user);
+        });
+
+      } else {
+
+        registerUser(params, function(err, resp){
+          if(resp == 'OK')
+            return done(null, params);
+        });
+
+      }
+
+    });
+
+  }
+
+  // exist user
+  function matchUser(params, next) {
+
+    db.exists(params.local, next);
+
+  }
+
+  // login user
+  function loginUser(params, next) {
+
+    db.get(params.local, next);
+
+  }
+
+  // register user
+  function registerUser(params, next) {
+
+    db.set(params.local, JSON.stringify(params), next);
+
+  }
+
+  // expose local function
+  user.matchUser = matchUser;
+  user.loginUser = loginUser;
+  user.registerUser = registerUser;
+
+  // legacy testing
   user.add = function(params, next) {
 
     console.log(params);
